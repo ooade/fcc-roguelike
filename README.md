9d7618173cf086db3db716d47f16381b3d3e4cba
Adegbuyi Ademola <marhyorh@gmail.com>
2016-06-17 16:46:34 +0100

    CCells generated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 FreeCodeCamp React Project - Roguelike Game
+
+__Algorithm__
+
+1. Start with a grid full of walls
+2. Pick a cell, mark it as part of the maze. Add the walls of the cell to the wall list.
+3. While there are walls in the list:
+  a. Pick a random wall from the list. We now have two cases: either there exists exactly one unvisited cell on one of the two sides of the chosen wall, or there does not. If it is the former:
+    i. Make the wall a passage and mark the unvisited cell as part of the maze.
+    ii. Add the neighboring walls of the cell to the wall list.
+  2. Remove the wall from the list.
--- src/components/dungeon.js
+++ src/components/dungeon.js
@@ -9,7 +9,8 @@ const widths = [ iwidth, 600, 700, 800 ];
 const height = heights[_.random(0, 3)];
 const width = widths[_.random(0, 3)];
 const cw = 10, ch = 10; // cell width && cell height
-const floor = [];
+const grid = [ width / cw, height / ch ];
+let floor = [], maze = [], rooms = 0;
 
 class Dungeon extends Component {
   componentWillMount() {
@@ -19,12 +20,65 @@ class Dungeon extends Component {
 
   renderFloors() {
     return _.map(floor, i => {
-      return <div className="floor cell" id={i} key={i} style={{ width: cw, height: ch }}></div>
+      return <div className={ i.type + " cell" } id={i.n} key={i.n} style={{ width: cw, height: ch }}></div>
     });
   }
 
+  renderMaze() {
+    _.map(maze, i => {
+      floor = _.uniqBy(_.concat(floor, [{ n: i, type: "maze"}]), 'n');
+      floor[i - 1] = { n: i, type: "maze"};
+    });
+    rooms++;
+  }
+
   generateTiles() {
-    console.log(floor[floor.length - 1]);
+    // Pick a cell, mark it as part of the maze.
+    let cell = _.random(50, 100);
+    maze.push(cell);
+    this.generateWalls(cell);
+    while (rooms < 30) {
+      cell = cell + _.random(40, 60);
+      maze.push(cell);
+      this.generateWalls(cell);
+    }
+  }
+
+  generateWalls(cell) {
+    // if the cell is at the right/left edge of the maze or around
+    if ((cell % grid[0]) + 6 >= grid[0]) {
+      //generateWalls to the leftside of the cell
+      let walls = [ cell, cell - 1, cell - 2, cell - 3, cell - 4, cell - 5, cell - 6];
+      maze = _.uniq(maze.concat(walls));
+    }
+    else if ((cell % grid[0]) < 6) {
+      //generateWalls to the rightside of the cell
+      let walls = [ cell, cell + 1, cell + 2, cell + 3, cell + 4, cell + 5, cell + 6];
+      maze = _.uniq(maze.concat(walls));
+    }
+    else {
+      //generateWalls both sides of the cell
+      let walls = [ cell, cell - 1, cell + 1, cell - 2, cell + 2, cell - 3, cell + 3];
+      maze = _.uniq(maze.concat(walls));
+    }
+
+    // console.log("cell alts", walls);
+    if (cell > 0 && cell < grid[0] * 3) {
+      //generateWalls below current cell
+      let walls = [ cell, cell + grid[0], cell + (grid[0] * 2), cell + (grid[0] * 3), cell + (grid[0] * 4) ];
+      maze = _.uniq(maze.concat(walls));
+    }
+    else if (grid[0] * 4 && cell < grid[0] * grid[1]) {
+      //generateWalls above current cell
+      let walls = [ cell, cell - grid[0], cell - (grid[0] * 2), cell - (grid[0] * 3), cell - (grid[0] * 4) ];
+      maze = _.uniq(maze.concat(walls));
+    }
+    else {
+      let walls = [ cell, cell - grid[0], cell + grid[0], cell - (grid[0] * 2), cell + (grid[0] * 2) ];
+      maze = _.uniq(maze.concat(walls));
+    }
+
+    this.renderMaze();
   }
 
   render() {
@@ -40,8 +94,8 @@ class Dungeon extends Component {
 
 function seedFloor() {
   /* Push exact number of cells to floor i.e fill the floor */
-  for (let i = 0; i < (width / cw) * (height / ch); i++) {
-    floor.push(i);
+  for (let i = 1; i <= (width / cw) * (height / ch); i++) {
+    floor.push({ n: i, type: "floor" });
   }
 }
 
