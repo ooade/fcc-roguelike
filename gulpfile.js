b071c3153aa8e943aef7a1fae76de814236907a5
Adegbuyi Ademola <marhyorh@gmail.com>
2016-07-05 10:36:08 +0100

    Added gulp :octocat: and sass included

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/** require gulp **/
+const gulp = require('gulp');
+
+/** require gulp-sass **/
+const sass = require('gulp-sass');
+
+/** Adds a task "sass" **/
+gulp.task('sass', () => {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./style'));
+});
+
+/** Adds a task "sass:watch" **/
+gulp.task('sass:watch', () => {
+  return gulp.watch('./sass/**/*.scss', ['sass']);
+});
